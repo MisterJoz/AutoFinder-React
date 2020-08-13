63618b00fce0ffe6d132cc6b3c6775a4191bd224
Garza <Josh.Garza@cbre.com>
2020-08-13 09:25:44 -0500

    "update collection preview"

---
+++ src/pages/shop/shopdata.js
@@ -0,0 +1,217 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Trucks",
+    routeName: "Trucks",
+    items: [
+      {
+        id: 1,
+
+        name: "Ford F-150",
+        imageUrl:
+          "https://images.unsplash.com/photo-1577061426123-3bcb5dfdf1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        price: 30,
+      },
+      {
+        id: 2,
+        name: "Chevrolet Silverado",
+        imageUrl:
+          "https://images.cars.com/cldstatic/wp-content/uploads/chevrolet-silverado-2500-2020-18-angle--exterior--front--silver.jpg",
+        price: 30,
+      },
+      {
+        id: 3,
+
+        name: "Ford F-150",
+        imageUrl:
+          "https://images.unsplash.com/photo-1577061426123-3bcb5dfdf1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        price: 30,
+      },
+      {
+        id: 4,
+        name: "Chevrolet Silverado",
+        imageUrl:
+          "https://images.cars.com/cldstatic/wp-content/uploads/chevrolet-silverado-2500-2020-18-angle--exterior--front--silver.jpg",
+        price: 30,
+      },
+      {
+        id: 5,
+
+        name: "Ford F-150",
+        imageUrl:
+          "https://images.unsplash.com/photo-1577061426123-3bcb5dfdf1f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        price: 30,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "SUVS",
+    routeName: "SUVs",
+    items: [
+      {
+        id: 1,
+        name: "Mercedes GLE 450",
+        imageUrl:
+          "https://www.motortrend.com/uploads/sites/5/2019/06/2020-Mercedes-Benz-GLE-450-front-three-quarter-in-motion-2.jpg",
+        price: 75,
+      },
+      {
+        id: 2,
+        name: "Cadillac Escalade",
+        imageUrl:
+          "https://media.ed.edmunds-media.com/cadillac/escalade-esv/2021/oem/2021_cadillac_escalade-esv_4dr-suv_premium-luxury-platinum_fq_oem_1_500.jpg",
+        price: 80,
+      },
+      {
+        id: 3,
+        name: "Mercedes GLE 450",
+        imageUrl:
+          "https://www.motortrend.com/uploads/sites/5/2019/06/2020-Mercedes-Benz-GLE-450-front-three-quarter-in-motion-2.jpg",
+        price: 75,
+      },
+      {
+        id: 4,
+        name: "Cadillac Escalade",
+        imageUrl:
+          "https://media.ed.edmunds-media.com/cadillac/escalade-esv/2021/oem/2021_cadillac_escalade-esv_4dr-suv_premium-luxury-platinum_fq_oem_1_500.jpg",
+        price: 80,
+      },
+      {
+        id: 5,
+        name: "Cadillac Escalade",
+        imageUrl:
+          "https://media.ed.edmunds-media.com/cadillac/escalade-esv/2021/oem/2021_cadillac_escalade-esv_4dr-suv_premium-luxury-platinum_fq_oem_1_500.jpg",
+        price: 80,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Sedans",
+    routeName: "Sedans",
+    items: [
+      {
+        id: 1,
+        name: "Mercedes S550",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/17q1/674167/2017-mercedes-benz-s550-4matic-test-review-car-and-driver-photo-677610-s-original.jpg?fill=2:1&resize=1200:*",
+        price: 125,
+      },
+      {
+        id: 2,
+        name: "Audi S5",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-audi-s5-sportback-mmp-1-1591045137.jpg",
+        price: 60,
+      },
+      {
+        id: 3,
+        name: "Mercedes S550",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/17q1/674167/2017-mercedes-benz-s550-4matic-test-review-car-and-driver-photo-677610-s-original.jpg?fill=2:1&resize=1200:*",
+        price: 125,
+      },
+      {
+        id: 4,
+        name: "Audi S5",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-audi-s5-sportback-mmp-1-1591045137.jpg",
+        price: 60,
+      },
+      {
+        id: 5,
+        name: "Mercedes S550",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/17q1/674167/2017-mercedes-benz-s550-4matic-test-review-car-and-driver-photo-677610-s-original.jpg?fill=2:1&resize=1200:*",
+        price: 125,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Coupes",
+    routeName: "coupes",
+    items: [
+      {
+        id: 1,
+        name: "Mustang Shelby GT350",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-ford-mustang-shelby-gt350r-101-1565613876.jpg?crop=0.819xw:1.00xh;0.0929xw,0&resize=768:*",
+        price: 70,
+      },
+      {
+        id: 2,
+        name: "BMW M4 CS",
+        imageUrl:
+          "https://media.ed.edmunds-media.com/bmw/m4-cs/2019/oem/2019_bmw_m4-cs_coupe_base_fq_oem_1_1600.jpg",
+        price: 100,
+      },
+      {
+        id: 3,
+        name: "Mustang Shelby GT350",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-ford-mustang-shelby-gt350r-101-1565613876.jpg?crop=0.819xw:1.00xh;0.0929xw,0&resize=768:*",
+        price: 70,
+      },
+      {
+        id: 4,
+        name: "BMW M4 CS",
+        imageUrl:
+          "https://media.ed.edmunds-media.com/bmw/m4-cs/2019/oem/2019_bmw_m4-cs_coupe_base_fq_oem_1_1600.jpg",
+        price: 100,
+      },
+      {
+        id: 5,
+        name: "Mustang Shelby GT350",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-ford-mustang-shelby-gt350r-101-1565613876.jpg?crop=0.819xw:1.00xh;0.0929xw,0&resize=768:*",
+        price: 70,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Exotics",
+    routeName: "Exotics",
+    items: [
+      {
+        id: 1,
+        name: "Lamborghini Aventador SVJ",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/518275-1535116629.jpg",
+        price: 400,
+      },
+      {
+        id: 2,
+        name: "Porsche 992 911",
+        imageUrl:
+          "https://i.insider.com/5bfea971d1e8445ab7249ae9?width=1100&format=jpeg&auto=webp",
+        price: 140,
+      },
+      {
+        id: 3,
+        name: "Lamborghini Aventador SVJ",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/518275-1535116629.jpg",
+        price: 400,
+      },
+      {
+        id: 4,
+        name: "Porsche 992 911",
+        imageUrl:
+          "https://i.insider.com/5bfea971d1e8445ab7249ae9?width=1100&format=jpeg&auto=webp",
+        price: 140,
+      },
+      {
+        id: 5,
+        name: "Lamborghini Aventador SVJ",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/518275-1535116629.jpg",
+        price: 400,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
